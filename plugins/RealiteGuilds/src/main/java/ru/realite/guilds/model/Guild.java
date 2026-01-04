9bcda37ad0724911b2bcc2ec6fba7f38267ee311
perm1ss10n <perm1ss10n@yandex.ru>
2026-01-04 17:36:24 +0300

    Add guild upgrade level storage

--- plugins/RealiteGuilds/src/main/java/ru/realite/guilds/model/Guild.java
+++ plugins/RealiteGuilds/src/main/java/ru/realite/guilds/model/Guild.java
@@ -1,6 +1,8 @@
 package ru.realite.guilds.model;
 
+import java.util.Map;
 import java.util.UUID;
 
-public record Guild(String tag, String name, UUID owner, GuildHome home, GuildClaim claim, int level, long xp) {
+public record Guild(String tag, String name, UUID owner, GuildHome home, GuildClaim claim, int level, long xp,
+                    Map<String, Integer> upgradeLevels) {
 }
