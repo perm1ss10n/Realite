d61fdd888cefa66b5c57bda0e6149e28f7c2caf2
perm1ss10n <perm1ss10n@yandex.ru>
2026-01-09 00:34:56 +0300

    Add player spell service API

---
+++ plugins/RealiteMagic/src/main/java/ru/realite/magic/service/PlayerSpellService.java
@@ -0,0 +1,20 @@
+package ru.realite.magic.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PlayerSpellService {
+
+    boolean hasSpell(UUID playerId, String spellId);
+
+    UnlockResult unlock(UUID playerId, String spellId, UnlockCause cause);
+
+    RevokeResult revoke(UUID playerId, String spellId, UnlockCause cause);
+
+    List<String> listLearned(UUID playerId);
+
+    Optional<String> getSelected(UUID playerId);
+
+    SelectResult select(UUID playerId, String spellId);
+}
